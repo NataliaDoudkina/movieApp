63fe3ca936515e24cafa6c68f2c228ed08d73433
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2021-01-16 18:26:19 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path=require("path");
+
+
+
+module.exports=(env)=>{
+return{
+    entry:"./src/index.js",
+    output:{
+        path:path.join(__dirname,'public'),
+        filename:"bundle.js"
+    },
+    module:{
+        rules:[
+            { 
+                test: /\.css?$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader" },
+                ]
+              },
+              {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+              {
+                test: /\.scss$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader" },
+                  { loader: "sass-loader" }
+                ]
+              },           
+               
+            {
+                test:/\.js$/,
+                exclude: [/node_modules/],
+                loader: 'babel-loader',
+              
+
+            }
+        ]
+    },
+   
+    devtool:'cheap-module-source-map',
+    devServer:{
+        contentBase: path.join(__dirname,'public')
+    }
+  }
+}
