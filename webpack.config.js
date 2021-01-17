0d7520c1451318b0c4216d1e893a0fdac5b9db6c
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2021-01-16 20:14:19 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path=require("path");
 
 module.exports={
   mode: 'production',
-    entry:{
-      app:"./src/index.js"
-    },
+     entry:"../src/index.js",
     output:{
         path:path.join(__dirname,'public'),
         filename:"bundle.js"
